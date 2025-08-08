a415e997b3c489312b7aaad9e10c295c78722679
Fomafix <fomafix@googlemail.com>
2025-08-08 21:57:02 +0000

    Remove second parameter for constructor for JsonContentHandler
    Before I5c47b56037bf36a67da904c7c6327e76e05df9b2 there was no second
    parameter in JsonContentHandler and therefor the second parameter was
    ignored. Since I5c47b56037bf36a67da904c7c6327e76e05df9b2 the second
    parameter is used for service dependency injection and therefor this
    call fail with:
    
    TypeError: MediaWiki\Content\JsonContentHandler::__construct():
    Argument #2 ($parsoidParserFactory) must be of type
    ?MediaWiki\Parser\Parsoid\ParsoidParserFactory, array given, called in
    /workspace/src/extensions/VisualData/includes/content/
    VisualDataJsonDataContentHandler.php on line 8
    
    Change-Id: I8d2aa946f5ceaa17ded360520362e9b34ca04721

--- includes/content/VisualDataJsonDataContentHandler.php
+++ includes/content/VisualDataJsonDataContentHandler.php
@@ -5,7 +5,7 @@ class VisualDataJsonDataContentHandler extends \JsonContentHandler {
 	 * @inheritDoc
 	 */
 	public function __construct( $modelId = CONTENT_MODEL_VISUALDATA_JSONDATA ) {
-		parent::__construct( $modelId, [ CONTENT_FORMAT_JSON ] );
+		parent::__construct( $modelId );
 	}
 
 	/**
